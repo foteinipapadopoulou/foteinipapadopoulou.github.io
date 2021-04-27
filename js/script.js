296897ddfa131b8735a1b7f8d02fa4efcfaac47c
Foteini Papadopoulou <34911926+foteinipapadopoulou@users.noreply.github.com>
2021-04-27 23:45:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,7 +42,10 @@ function sendData(){
   } );
 
   // Set up our request
+    
   XHR.open( "POST", "https://formcarry.com/s/mprp5IvSAS3");
+    XHR.setRequestHeader('Content-Type', 'application/json');
+    XHR.setRequestHeader('Accept', 'application/json');
   // The data sent is what the user provided in the form
   XHR.send( FD );
 }
