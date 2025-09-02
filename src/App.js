98a5505ebe28d558d3d55a2419306ca79c1f7979
Foteini Papadopoulou <34911926+foteinipapadopoulou@users.noreply.github.com>
2025-09-02 17:40:49 +0200

    Remove commented-out Route for navigation

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          // <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
       </div>
