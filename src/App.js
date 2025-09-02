5f1ea306f7b1aa98073467e2ad22025a9ee74c07
Foteini Papadopoulou <34911926+foteinipapadopoulou@users.noreply.github.com>
2025-09-02 17:28:11 +0200

    Change Router from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact/Contact";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
